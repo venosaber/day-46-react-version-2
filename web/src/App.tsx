cb8e4df7ba0a7f6b62d8fe894764ff6bc52780be
bangtx <banhtran.hha@gmail.com>
2025-04-21 21:09:30 +0700

    inir web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
 import './App.css'
+import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+import Paper from '@mui/material/Paper';
+
+interface Header {
+    name: string
+    text: string
+}
+
+interface Row {
+    id: number
+    name: string
+    age: number
+    address: string
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
+    const headers: Header[] = [
+        { name: 'id', text: 'ID' },
+        { name: 'name', text: 'Ten' },
+        { name: 'age', text: 'Tuoi' },
+        { name: 'address', text: 'Dia Chi' },
+    ]
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const rows: Row[] = [
+        { id: 1, name: 'Dung', age: 20, address: 'Thanh Oai - Ha Noi' },
+        { id: 2, name: 'Trung', age: 22, address: 'Quoc Oai - Ha Noi' },
+    ]
+
+
+    return (
+        <>
+            <h1>hello anh em</h1>
+            <TableContainer sx={{ width: 650, margin: 'auto' }} component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            {
+                                headers.map((header: Header) => {
+                                    return <TableCell key={header.name}>{header.text}</TableCell>
+                                })
+                            }
+                        </TableRow>
+                    </TableHead>
+
+                    <TableBody>
+                        {
+                            rows.map((row: Row) => {
+                                return (
+                                    <TableRow key={row.id}>
+                                        {
+                                            Object.keys(row).map((rowKey: string) => {
+                                                return (
+                                                    <TableCell key={`${rowKey}-${row.id}`}>{row[rowKey]}</TableCell>
+                                                )
+                                            })
+                                        }
+                                    </TableRow>
+                                )
+                            })
+                        }
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </>
+    )
 }
 
 export default App
